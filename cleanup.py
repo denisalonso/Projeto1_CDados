ce280305406bdfd530d512709c0eb7d637057b35
Denis Alonso <denisalonso1999@gmail.com>
2024-09-12 11:55:28 -0300

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,3 +1,17 @@
+txt_physics = ''
+txt_chemistry = ''
+txt_biology = ''
+for i in range(len(treino.Comment)):
+    if treino.Topic[i] =='Physics':
+        txt_physics += treino.Comment[i]
+        txt_physics += ' '
+    elif treino.Topic[i] =='Chemistry':
+        txt_chemistry += treino.Comment[i]
+        txt_chemistry += ' '
+    else:
+        txt_biology += treino.Comment[i]
+        txt_biology += ' '
+
 def cleanup(txt):
     new_txt = ''
     clean_txt = txt.replace('\\n',' ')
